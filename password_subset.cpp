8371b78148d5d55da6f7329fe2104ce83e97141b
ndijkhoffz <ndijkhoffz@neptunetg.com>
2018-05-04 09:07:00 -0500

    Algorithms:
    This was a test that I completed. I had 45 minutes to figure out how to
    obtain the largest subset string that did not contain digits and had to
    contain a uppercase character. This example serves as reference for the
    cpp language as well as parsing through a string for specific characters. The use of standard functions such as isupper() and is islower() improve efficiency and readability.

---
+++ password_subset.cpp
@@ -0,0 +1,61 @@
+// my first program in c++
+#include <iostream>
+using namespace std;
+
+int solution(string &S);
+
+int main()
+{
+	string str = "laskjflkGkslGa0fa1adHj5";
+    
+    solution(str);
+    
+    return 0;
+}
+
+
+int solution(string &S) 
+{
+    // write your code in C++14 (g++ 6.2.0)
+	int n = 0;
+	int current_max_subset = 0;
+	int largest_max_subset_found = 0;
+	bool uppercase_in_subset = false;
+    
+    while(S.length() >= n)
+    {
+        if(isupper(S[n]))
+        {
+        	current_max_subset += 1;
+        	if(!uppercase_in_subset)
+        	{
+        		uppercase_in_subset = true;
+			}
+        }
+        
+        // check if char is lower case
+        if(islower(S[n]))
+        {
+        	current_max_subset += 1;        	
+		}
+		
+		if(!(isupper(S[n])) && !(islower(S[n])))
+		{
+			if (largest_max_subset_found < current_max_subset)
+			{
+				if(uppercase_in_subset)
+				{
+					largest_max_subset_found = current_max_subset;
+				}
+				// reset uppercase flag
+				uppercase_in_subset = false;
+				current_max_subset = 0;
+			}
+		}
+		
+		//increase counter
+		++n;
+	}
+
+    cout << largest_max_subset_found;
+}
